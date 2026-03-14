cc3b203a5340e3e6b7060bc928ed62f576f1dd6c
yangqi-huang <huangharry880@outlook.com>
2026-03-14 10:03:36 +0000

    practice

---
+++ pipeline/ingest_data3.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from sqlalchemy import create_engine
+from tqdm.auto import tqdm
+import click
+
+@click.command()
+@click.option('--pg-user', default='root', help='PostgreSQL user')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default=5432, type=int, help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, target_table):
+    
+    url = 'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2025-11.parquet'
+
+    engine = create_engine(f'postgresql+psycopg://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
+
+    df = pd.read_parquet(url)
+    df.head(0).to_sql(name=target_table, con=engine, if_exists='replace')
+    df.to_sql(name=target_table, con=engine, if_exists="append")
+            
+if __name__ == '__main__':
+    run()
